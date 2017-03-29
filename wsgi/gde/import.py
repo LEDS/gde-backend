d31e69a8ed5cd5ee2d81ea4bb338857950fbfd70
lukasg18 <lukas.gomes@hotmail.com>
2017-03-29 17:38:03 -0300

    script pra carregar arquivo CSV para o banco feito

--- wsgi/gde/import.py
+++ wsgi/gde/import.py
@@ -12,12 +12,38 @@ django.setup()
 
 from app.models import Setor, Campus
 
-data = csv.reader(open("/home/lucas/LEDs/gdeSSH/setores.csv"),delimiter=",")
+data = csv.reader(open("/home/lucas/LEDs/gdeSSH/setor.csv"),delimiter=",")
+
+# campus = Campus()
+# setor = Setor()
+# campus.nome = "Serra"
+# campus = Campus.objects.all()
+# setor.campus = Campus.objects.get(pk=campus[0].id)
+# setor.nome = "Biblioteca"
+# setor.save()
 
 for row in data:
 	if row[0] != 'ID_UNIDADE':
+		campus = Campus()
 		setor = Setor()
-		campus = Campus.objects.get(pk=1)
-		setor.campus = campus
-		setor.nome = row[2]
-		setor.save()
+		setor.nome = row[5]
+		campus.nome = row[4]
+		if Campus.objects.filter(nome=campus.nome).exists():
+			# campus = Campus.objects.get(nome=campus.nome)
+			# print(campus.id)
+			campus = Campus.objects.get(nome=campus.nome)
+			campus = Campus.objects.get(pk=campus.id)
+			setor.campus = campus
+			setor.save()
+		else:
+			campus.save()
+			campus = Campus.objects.get(nome=campus.nome)
+			campus = Campus.objects.get(pk=campus.id)
+			setor.campus = campus
+			setor.save()
+
+		# setor = Setor()
+		# campus = Campus.objects.get(pk=1)
+		# setor.campus = campus
+		# setor.nome = row[2]
+		# setor.save()
