1e9ae0b8e2b7f8d040d9ebc6e9ec3f0a18da22c3
lukasg18 <lukas.gomes@hotmail.com>
2017-04-11 12:04:15 -0300

    corrigido o erro do logout

--- wsgi/gde/gde/urls.py
+++ wsgi/gde/gde/urls.py
@@ -23,7 +23,10 @@ from app.views import *
 from app import admin as adminMethod
 
 urlpatterns = [
-                  url('^', include('django.contrib.auth.urls')),
+                  url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+                  url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+                  url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
+                  url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
                   url(r'^admin/', admin.site.urls),
                   url(r'^register/$', cadastroUsuario, name='cadastro_usuario'),
                   url(r'^home/$', home),
